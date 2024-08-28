ea92f2ae866f717a68ba8500ee4c210cf8f2776a
molaraiche <laraichemohamed@gmail.com>
2024-08-28 20:34:19 +0100

    init

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Landify | molaraiche',
+  description:
+    'Landify is a phone landing page created by Mohamed laraiche (molaraiche.com), working with next js,typescript tailwind to present and show skills in using those awesome technologies.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    colors: {
+      neutral: {
+        100: '#F3F4F6',
+        200: '#E5E7EB',
+        300: '#D1D5DB',
+        500: '#6B7280',
+        600: '#A6A6A6',
+        700: '#374151',
+        800: '#33475B',
+        900: '#111827',
+      },
+      primary: {
+        100: '#EDE9FE',
+        300: '#75E3EA',
+        600: '#1DB5BE',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
